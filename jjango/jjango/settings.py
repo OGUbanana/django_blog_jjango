d348e15613bca7de9d29758713e6d92a3d7b603f
Youngmanbo <yo900326@gmail.com>
2023-09-11 17:40:10 +0900

    related to: #2

--- jjango/jjango/settings.py
+++ jjango/jjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+with open(os.path.dirname(os.path.realpath(__file__)) + '\config.json', 'r') as f:
+    json_data = json.load(f)
+    db_key = json_data['POSTGRESQL_KEY']
+    secret_key = json_data['DJANGO_SECRET_KEY']
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--fx8o-5h&xep$^3m-_)n&1%ec@&n=(629n(er*-4=8#c_e_hum'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,10 +82,14 @@ WSGI_APPLICATION = 'jjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'blog',
+        'USER': 'postgres',
+        'PASSWORD': db_key
     }
 }
 
