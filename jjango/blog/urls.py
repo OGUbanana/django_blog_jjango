d4026ef88921c1a66dec5d4b4c1e254ea1f41368
LeeYeonghwan <129285372+Leeeyounghwan@users.noreply.github.com>
2023-09-11 17:43:06 +0900

    feat: views.py 기능 추가 (#5)
    urls.py: path 추가
    views.py: login, logout, write 메서드 추가
    
    Resolves: #6

--- jjango/blog/urls.py
+++ jjango/blog/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+    path('write/', views.write, name="write"),
+    path('board/', views.board, name="board"),
+    path("signup/", views.signup, name="signup"),
 ]
--- jjango/jjango/settings.py
+++ jjango/jjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +75,20 @@ WSGI_APPLICATION = 'jjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+with open('config.json', 'r') as f:
+    json_data = json.load(f)
+    db_key = json_data['POSTGRESQL_KEY']
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "blog",
+        'USER': "postgres",
+        "PASSWORD" : db_key,
+        "HOST" : "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
