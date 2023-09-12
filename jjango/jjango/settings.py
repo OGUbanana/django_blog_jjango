9c4190f8f6daf3fc94fe41a78db29d4e09c89941
OGUbanana <114931412+OGUbanana@users.noreply.github.com>
2023-09-12 09:49:36 +0900

    Update settings.py

--- jjango/jjango/settings.py
+++ jjango/jjango/settings.py
@@ -21,16 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-with open(os.path.dirname(os.path.realpath(__file__)) + '\config.json', 'r') as f:
-    json_data = json.load(f)
-    db_key = json_data['POSTGRESQL_KEY']
-    secret_key = json_data['DJANGO_SECRET_KEY']
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
